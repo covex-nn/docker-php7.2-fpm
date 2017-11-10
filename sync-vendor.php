55b1cfa73bc2ee0fad1acc2ea6f768f34d32d98a
Andrey F. Mindubaev <covex.mobile@gmail.com>
2017-11-10 18:49:52 +0300

    Add unique stub

--- sync-vendor.php
+++ sync-vendor.php
@@ -5,29 +5,32 @@ $source = '/composer/vendor';
 $target = '/srv/.sync';
 
 if (is_file($target) && !is_dir($target)) {
-  unlink($target);
+    unlink($target);
 }
 if (!file_exists($target)) {
-  mkdir($target, true);
+    mkdir($target, true);
 }
 if (file_exists($source) && is_dir($source)) {
-  foreach (new DirectoryIterator($source) as $dir) {
-    /* @var $dir DirectoryIterator */
-    if (!$dir->isDot() && $dir->isDir()) {
-      $pharName = $dir->getFilename();
-      if (in_array($pharName, ["bin"])) {
-        continue;
-      }
-      $pharAlias = $pharName . ".phar";
-      $pharFilename = $target . "/" . $pharAlias;
+    foreach (new DirectoryIterator($source) as $dir) {
+        /* @var $dir DirectoryIterator */
+        if (!$dir->isDot() && $dir->isDir()) {
+            $pharName = $dir->getFilename();
+            if (in_array($pharName, ["bin"])) {
+                continue;
+            }
+            $pharAlias = $pharName . ".phar";
+            $pharFilename = $target . "/" . $pharAlias;
 
-      $phar = new Phar($pharFilename);
-      try {
-        $phar->buildFromDirectory($dir->getPathname());
-        echo "Synced vendor $pharAlias\n";
-      } catch (Exception $exception) {
-        echo "Could not sync vendor $pharAlias: " . $exception->getMessage() . "\n";
-      }
+            $phar = new Phar($pharFilename);
+            try {
+                $phar->buildFromDirectory($dir->getPathname());
+                $phar->setStub(
+                        "<?php /* createdAt " . date("Y-m-d H:i:s") . " */ __HALT_COMPILER();" . PHP_EOL
+                );
+                echo "Synced vendor $pharAlias\n";
+            } catch (Exception $exception) {
+                echo "Could not sync vendor $pharAlias: " . $exception->getMessage() . "\n";
+            }
+        }
     }
-  }
 }
