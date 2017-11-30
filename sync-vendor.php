36d42676f4b27ecf3756a2ca40f01f6ebb0eca6a
Andrey F. Mindubaev <covex.mobile@gmail.com>
2017-11-30 09:18:56 +0300

    Self-extracting synced phars

--- sync-vendor.php
+++ sync-vendor.php
@@ -25,8 +25,37 @@ if (file_exists($source) && is_dir($source)) {
             try {
                 $phar->buildFromDirectory($dir->getPathname());
                 $phar->setStub(
-                        "<?php /* createdAt " . date("Y-m-d H:i:s") . " */ __HALT_COMPILER();" . PHP_EOL
+                        '<?php /* ' . date("Y-m-d H:i:s") . ' */
+
+if (file_exists("' . $pharName . '")) {
+    if (is_file("' . $pharName . '")) {
+        unlink("' . $pharName . '");
+    } elseif (is_dir("' . $pharName . '")) {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator("' . $pharName . '", FilesystemIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        foreach ($iterator as $filename => $fileInfo) {
+            if ($fileInfo->isDir()) {
+                rmdir($filename);
+            } else {
+                unlink($filename);
+            }
+        }
+    }
+}
+if (!file_exists("' . $pharName . '")) {
+    $phar = new Phar(__FILE__);
+    $phar->extractTo("' . $pharName . '", null, true);
+    unset($phar);
+    unlink(__FILE__);
+} else {
+    echo "Could not extract PHAR. File \"' . $pharName . '\" exists" . PHP_EOL;
+}
+__HALT_COMPILER();
+'
                 );
+                chmod($pharFilename, 0755);
                 echo "Synced vendor $pharAlias\n";
             } catch (Exception $exception) {
                 echo "Could not sync vendor $pharAlias: " . $exception->getMessage() . "\n";
